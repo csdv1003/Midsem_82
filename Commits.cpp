5e4db7c4090969747982154a22e984c744a5a42c
RAKSHIT KAPOOR <500067642@DDN.UPES.AC.IN>
2019-03-18 12:35:19 +0530

    first commit made

---
+++ Commits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+main() {
+   int a = 21;
+   int c ;
+ 
+
+   c = a++;   
+   cout << "Line 1 - Value of a++ is :" << c << endl ; // value will not be increased.
+ 
+  
+   cout << "Line 2 - Value of a is :" << a << endl ;
+ 
+
+   c = ++a;  
+   cout << "Line 3 - Value of ++a is  :" << c << endl ;
+   return 0;
+}
