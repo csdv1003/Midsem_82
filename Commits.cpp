4b0169fa849f1421dc186effa92d73be774685b7
RAKSHIT KAPOOR <500067642@DDN.UPES.AC.IN>
2019-03-18 12:38:12 +0530

    third commit added

--- Commits.cpp
+++ Commits.cpp
@@ -10,10 +10,10 @@ main() {
    cout << "Line 1 - Value of a++ is :" << c << endl ; // value will not be increased.
  
   
-   cout << "Line 2 - Value of a is :" << a << endl ; // value of a is increased 
+   cout << "Line 2 - Value of a is :" << a << endl ; // value of a is increased.
  
 
    c = ++a;  
-   cout << "Line 3 - Value of ++a is  :" << c << endl ;
+   cout << "Line 3 - Value of ++a is  :" << c << endl ; // value of a is further increased.
    return 0;
 }
